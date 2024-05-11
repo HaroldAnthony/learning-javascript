9a5fb9bd3061473fa19bdce0579d8200bd78d3c2
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-05-11 16:21:34 +0530

    string manipulation done with

---
+++ stringmethods.js
@@ -0,0 +1,13 @@
+var str1 = "Constantinople";
+var try1 = str1.length;
+
+var str2 = "Harry is a man";
+var try2 = str2.indexOf("man");
+
+var str3 = "bus, car, plane, boat";
+var try3 = str3.slice(3,21);
+
+
+var str4 = "Please come to my house";
+var try4 = str4.replace("house","home");
+console.log(try4);
