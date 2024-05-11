b22925c4f2b826acc86d5d4a3e1039df09149f86
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-05-11 15:09:53 +0530

    done with variables

--- variables.js
+++ variables.js
@@ -1,2 +1,7 @@
-var carcolor = "blue";
-console.log(carcolor);
+var carcolor1 = "red";
+var carcolor2 = "blue";
+var carcolor3 = "green";
+var num1 = 5;
+var num2 = 7;
+var bool = true;
+console.log(bool);
