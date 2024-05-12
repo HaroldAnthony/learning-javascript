f01acaa54684a16a400052881bde9609ed6cfe7b
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-05-12 14:20:11 +0530

    learnt number operations

--- numbers.js
+++ numbers.js
@@ -1,1 +1,13 @@
-var 
+var a = 20;
+var b = 30+10;
+var c = 40;
+
+var d = a+b/c+2*4-(a*b*c);
+
+var e = 7.8342726
+var f = e.toPrecision(4);
+var g = e.toPrecision();
+
+var h = 732;
+var j = h.toString();
+console.log(h+32);
