0aa1103eed671a9454d30dc7fbda640256ba4ab5
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-05-14 20:44:09 +0530

    done with arr

--- arrays.js
+++ arrays.js
@@ -1,4 +1,11 @@
-var colors = ["red","blue","green","yellow","pink"];
-colors[2] = "purple"
-var c1 = colors[2];
+//var colors = ["red","blue","green","yellow","pink"];
+//colors[2] = "purple";
+//var c1 = colors[2];
+//var c2 = colors.length;
 //console.log(c1);
+//console.log(c2);
+
+var values = ["red",6,3,true,"pink"];
+//var v1 = values[1]+values[2];
+var v2 = values[5];
+console.log(v2);
