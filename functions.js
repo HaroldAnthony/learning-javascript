a0a2eb29c08595dd0d6874d68022f05ff9bc889d
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-05-17 12:21:36 +0530

    done with functions

--- functions.js
+++ functions.js
@@ -2,4 +2,11 @@ function food(item){
     console.log("My favourite food is "+item);
 }
 
+function order(quantity,price){
+    console.log("The quantity of order is "+quantity+" kgs");
+    console.log("The price of each kg is "+price+" rupees");
+    console.log("The total of the bill is "+quantity*price+" rupees");
+}
+
 food("biryani");
+order(2,120);
